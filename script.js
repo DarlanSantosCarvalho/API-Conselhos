0ff09c3952d658dcd0601740d220907bd8a95651
Darlan Carvalho <darlan171201@gmail.com>
2022-11-17 13:50:02 -0300

    Completei a requisição de API. Está funcionando perfeitamente e está responsivo.

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
+document.querySelector('.gerar-conselho').addEventListener('click', function(){
+    gerarConselho()
+})
+
+
 async function aconselhar() {
     const response = await fetch(`https://api.adviceslip.com/advice`)
-    console.log(await response.json())
+    return await response.json()
 }
 
 function gerarConselho() {
     aconselhar().then(dadosConselho => {
-        let conselhoGerado = `<h1> ${dadosConselho.advice} </h1>`
-        document.getElementById('conselho').innerHTML = `${conselhoGerado}`
+        let conselhoGerado = `<p> Advice #${dadosConselho.slip.id} </p>
+        <h1> ${dadosConselho.slip.advice} </h1>`
+        document.querySelector('.conselho').innerHTML = `${conselhoGerado}`
     })
 }
 
-
-aconselhar()
-
 gerarConselho()
+
+
+
